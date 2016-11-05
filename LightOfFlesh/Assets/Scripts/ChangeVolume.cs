aa153f8195a2bf97fc1e1fc9c59211ffbdbdfce1
Maxime Vinchon <maxime.vinchon@telecom-sudparis.eu>
2016-11-05 21:11:21 +0100

    Commit des trucs

---
+++ LightOfFlesh/Assets/Scripts/ChangeVolume.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeVolume : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Slider volumeSlider;
+    public AudioSource volumeAudio;
+    public void VolumeController()
+    {
+        volumeAudio.volume = volumeSlider.value;
+    }
+}
---
+++ LightOfFlesh/Assets/Scripts/ecranTitre.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ecranTitre : MonoBehaviour
+{   [SerializeField]
+    public string scene;
+    void OnGUI()
+    {
+        // Make a background box
+        GUI.Box(new Rect(200, 100, 400, 400), "Menu");
+
+        // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+        if (GUI.Button(new Rect(300, 150, 200, 100), "Jouer"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        // Make the second button.
+        if (GUI.Button(new Rect(300, 250, 200, 100), "Quiter"))
+        {
+            Application.Quit();
+        }
+    }
+}
