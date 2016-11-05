80d457eee92bd3b9f90969d102617d384636a28b
TabouretFantome <vinchon.maxime@gmail.com>
2016-11-05 21:15:34 +0100

    Add files via upload
    Les scripts pour le menu

---
+++ LightOfFlesh/Assets/Scripts/selectLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class selectLevel : MonoBehaviour {
+
+    [SerializeField]
+    public string destination;
+    public void loadLevel()
+    {
+        SceneManager.LoadScene(destination);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
