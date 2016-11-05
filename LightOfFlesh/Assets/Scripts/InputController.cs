1c8efbbebe3c6a9078ede6eee8e8d4ab6cf1ffa8
David THERY-BULHA <David THERY-BULHA>
2016-11-05 03:08:09 +0100

    tests lumières

---
+++ LightOfFlesh/Assets/Scripts/InputController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 speed;
+
+    [SerializeField]
+    private float maxSpeed;
+
+    [SerializeField]
+    private Light torchlight;
+
+
+    public float MaxSpeed
+    {
+        get
+        {
+            return this.maxSpeed;
+        }
+        private set
+        {
+            this.maxSpeed = value;
+        }
+    }
+
+    public Vector2 Speed
+    {
+        get;
+        set;
+    }
+
+    public Vector2 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+        private set
+        {
+            transform.position = value;
+        }
+    }
+
+    void Update()
+    {
+        Speed = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector2 newPosition = Position + MaxSpeed * Speed * Time.deltaTime;
+
+        Position = newPosition;
+
+        float x = Input.GetAxis("RSX");
+        float y = Input.GetAxis("RSY");
+        
+
+        torchlight.transform.eulerAngles = new Vector3(
+            Mathf.Atan2(y, x) * Mathf.Rad2Deg,
+            -90,
+            90
+            );
+
+        /*print(torchlight.transform.eulerAngles.x);
+        print(torchlight.transform.eulerAngles.y);
+        print(torchlight.transform.eulerAngles.z);*/
+    }
+}
