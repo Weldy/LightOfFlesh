4725c1cb84f11ccad6d555e75da0a6946b95acb9
David THERY-BULHA <David THERY-BULHA>
2016-11-05 09:20:50 +0100

    travail de la nuit

---
+++ LightOfFlesh/Assets/Scripts/Player/Hunted.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hunted : Player {
+    [SerializeField]
+    Light torchlight;
+
+    private bool isOn;
+
+
+	// Use this for initialization
+	void Start () {
+        isOn = false;
+    }
+   void MoveHunted()
+    {
+        Position += new Vector2(Input.GetAxis("XBoxHorizontal"), Input.GetAxis("XBoxVertical")) * Speed * Time.deltaTime;
+
+    }
+    void TorchlightControl()
+    {
+        float x = Input.GetAxis("RSX");
+        float y = Input.GetAxis("RSY");
+
+        if (x == 0 && y == 0)
+        {
+            torchlight.transform.eulerAngles = new Vector3(
+            torchlight.transform.eulerAngles.x,
+            -90,
+            90
+            );
+        }
+        else
+        {
+            torchlight.transform.eulerAngles = new Vector3(
+            Mathf.Atan2(-y, -x) * Mathf.Rad2Deg,
+            -90,
+            90
+            );
+        }
+
+        if (Input.GetButton("Action"))
+        {
+            if (isOn)
+            {
+                torchlight.range = 0;
+                isOn = false;
+            }
+            else
+            {
+                torchlight.range = 10;
+                isOn = true;
+            }
+
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        MoveHunted();
+
+        //TODO déplacement progressif
+        TorchlightControl();
+    }
+}
+
---
+++ LightOfFlesh/Assets/Scripts/Player/Hunter.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hunter : Player {
+
+    [SerializeField]
+    private float sprintSpeed;
+    [SerializeField]
+    private float sprintDistance;
+    [SerializeField]
+    private float sprintDeviate;
+    //   [SerializeField]
+    //    private float maxSpeed;
+
+    private float currentSpeed;
+    private float sprintCounterX, sprintCounterY;
+    private float sensH, sensV;
+    // Use this for initialization
+    void Start () {
+        sensH = 1;
+        sensV = 1;
+        sprintCounterX = 0;
+        sprintCounterY = 0;
+	}
+    void MoveHunter()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        if (sprintCounterX+sprintCounterY > sprintDistance)
+        {
+            currentSpeed += (sprintCounterX+sprintCounterY - sprintDistance) * sprintSpeed;
+       
+        }
+        else
+        {
+            currentSpeed = Speed;
+        }
+        Position += new Vector2(x, y) * currentSpeed * Time.deltaTime;
+
+        if (x * sensH > 0)
+        {
+            if ((y < sprintDeviate && y >= 0) || (y > -sprintDeviate && y < 0))
+            {
+                sprintCounterX++;
+            }
+
+            else
+            {
+                sprintCounterX = 0;
+               
+            }
+        }
+        else
+        {
+            sensH *= -1;
+
+            sprintCounterX = 0;
+        }
+    
+        if (y * sensV > 0)
+        {
+            if ((x < sprintDeviate && x >= 0) || (x > -sprintDeviate && x < 0))
+            {
+                sprintCounterY++;
+            }
+
+            else
+            {
+                sprintCounterY = 0;
+            }
+        }
+        else
+        {
+            sensV *= -1;
+
+            sprintCounterY = 0;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        MoveHunter();
+    }
+}
