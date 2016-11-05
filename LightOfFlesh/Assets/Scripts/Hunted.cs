a17ad73b5abcdea7998d166d55026af8d49417e7
Cattant <may.cattant@telecom-sudparis.eu>
2016-11-05 03:29:41 +0100

    Add files via upload

---
+++ LightOfFlesh/Assets/Scripts/Hunted.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hunted : Player {
+    [SerializeField]
+    Light torchlight;
+
+    private Vector2 currentSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+  
+    // Update is called once per frame
+    void Update () {
+   
+           }
+}
---
+++ LightOfFlesh/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 position;
+    public Vector2 Position
+    {
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+    [SerializeField]
+    private float range;
+    [SerializeField]
+    private float speed;
+
+    public float Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+        set
+        {
+            this.speed = value;
+        }
+    }
+
+
+
+}
