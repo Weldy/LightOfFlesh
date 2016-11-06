8478766088a35e29fa1046736957bb4ef5c7cec1
Maxime Vinchon <maxime.vinchon@telecom-sudparis.eu>
2016-11-06 10:06:36 +0100

    Commit du matin, chagrin

---
+++ LightOfFlesh/Assets/Scripts/defilerImage.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class defilerImage : MonoBehaviour
+{
+    public string destination;
+    public GameObject texture1;
+    public GameObject texture2;
+    private int timer;
+    private bool lastWas1;
+    
+
+    void Start()
+    {
+        timer = 50;
+        Texture.Instantiate(texture1, new Vector2(0, 0), Quaternion.identity);
+        lastWas1 = true;
+        
+    }
+
+    void Update()
+    {
+        timer--;
+        if (timer == 0)
+        {
+            if (lastWas1 == true)
+            {
+                Instantiate(texture2, new Vector2(0, 0), Quaternion.identity);
+                lastWas1 = false;
+            }
+            else
+            {
+                Instantiate(texture1, new Vector2(0, 0), Quaternion.identity);
+                lastWas1 = true;
+            }
+            timer = 50;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(destination);
+        }
+
+    }
+
+
+}
---
+++ LightOfFlesh/Assets/Scripts/video.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+    [RequireComponent(typeof(AudioSource))]
+
+public class video : MonoBehaviour {
+
+        [SerializeField]
+        public string destination;
+
+    public MovieTexture movie;
+
+    public AudioSource audio;
+
+	// Use this for initialization
+	void Start () 
+    {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(destination);
+        }
+    }
+
+}
